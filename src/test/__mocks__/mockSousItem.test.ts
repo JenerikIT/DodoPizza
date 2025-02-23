3ac921a1df3e7797aa06765e66b5b535f4bbc1dd
AhmaWeb <ahmaweb@mail.ru>
2025-02-23 20:04:15 +0300

    remove Test

--- src/test/__mocks__/mockSousItem.test.ts
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import baseComponent from "../../pages/CardMore";
-describe("baseComponent component", () => {
-  test("base test", () => {
-    render(<baseComponent/>);
-  });
-});
